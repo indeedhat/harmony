082130b985564ef1c9e8e0e65a9e70b337d4e278
indeedhat <iam4423@gmail.com>
2022-07-30 17:22:38 +0100

    feat: logs go to file by default

--- cmd/harmony-hid/main.go
+++ cmd/harmony-hid/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/indeedhat/harmony/internal/app"
 	"github.com/indeedhat/harmony/internal/common"
 	"github.com/indeedhat/harmony/internal/config"
+	"github.com/indeedhat/harmony/internal/logger"
 )
 
 func main() {
+	verbose := flag.Bool("v", false, "print logs to screen rather than log file")
+	flag.Parse()
+
+	if !*verbose {
+		logger.UseConfigFile()
+	}
 
 	conf := config.Load()
 	if conf == nil {
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,16 +2,29 @@ package logger
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
+
+// UseConfigFile when called all log events will be saved to a log file instead of prinnted to the console
+func UseConfigFile() error {
+	logFile, err := os.OpenFile("harmony.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	log.SetOutput(logFile)
+	return nil
+}
 
 // Log simple data to the console
 func Log(category string, v ...any) {
 	args := append([]any{"[" + category + "] "}, v...)
-	fmt.Println(args...)
+	log.Println(args...)
 }
 
 // Logf logs formatted data to the console
 func Logf(category, format string, args ...any) {
 	format = "[" + category + "] " + format
-	fmt.Println(fmt.Sprintf(format, args...))
+	log.Println(fmt.Sprintf(format, args...))
 }
