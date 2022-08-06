69a831956ca3be02a1edf41f695ba70886c825cf
indeedhat <iam4423@gmail.com>
2022-08-06 18:35:54 +0100

    refactor: arrange screens in ui based on current transition zones

--- internal/net/server/router/router.go
+++ internal/net/server/router/router.go
@@ -1,8 +1,6 @@
 package router
 
 import (
-	"encoding/json"
-	"html/template"
 	"mime"
 
 	"github.com/foolin/goview"
@@ -10,8 +8,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 	"github.com/indeedhat/harmony/internal/common"
-	"github.com/indeedhat/harmony/internal/net/server/controllers/socket"
-	"github.com/indeedhat/harmony/internal/net/server/controllers/ui"
+	"github.com/indeedhat/harmony/internal/net/server/socket"
+	"github.com/indeedhat/harmony/internal/net/server/ui"
 	"github.com/indeedhat/harmony/internal/screens"
 )
 
@@ -28,16 +26,6 @@ func New(ctx *common.Context, serverUUID uuid.UUID, displays []screens.DisplayBo
 	viewsConfig := goview.DefaultConfig
 	viewsConfig.Root = "./web/views"
 	viewsConfig.DisableCache = true
-	viewsConfig.Funcs = template.FuncMap{
-		"json": func(data interface{}) string {
-			bytes, err := json.Marshal(data)
-			if err != nil {
-				return ""
-			}
-
-			return string(bytes)
-		},
-	}
 	router.HTMLRender = ginview.New(viewsConfig)
 
 	router.Static("/js", "./web/public/js")
--- web/public/js/harmony.js
+++ web/public/js/harmony.js
@@ -1,12 +1,73 @@
 const Harmony = groups => {
-    console.log("called")
     Alpine.data("groups", () => ({
         init() {
+            this.reposition();
             console.log(this.groups);
+        },
+
+        reposition() {
+            let done = [this.groups[0].id];
+
+            for (let i = 0; i < this.groups.length; i++) {
+                let group = this.groups[i];
+                console.log({ group })
+
+                for (let j = 0; j < group.transitions.length; j++) {
+                    let trans = group.transitions[j];
+                    if (~done.indexOf(trans.id)) {
+                        continue;
+                    }
+
+                    let neighbour = this.findMatchingTransition(group.id, trans.id);
+                        console.log({ neighbour })
+                    if (!neighbour) {
+                        continue;
+                    }
+
+                    let newPos = new Vector(
+                        group.pos.x + trans.pos.x - neighbour.pos.x,
+                        group.pos.y + trans.pos.y - neighbour.pos.y
+                    );
+
+                    let target = this.findNeighbour(trans.id);
+                    if (target) {
+                        target.pos = newPos;
+                        done.push(target.id);
+                    }
+                }
+            }
+        },
+
+        findMatchingTransition(self, neighbour) {
+            for (let i = 0; i < this.groups.length; i++) {
+                let group = this.groups[i];
+                if (group.id != neighbour) {
+                    continue;
+                }
+
+                for (let j = 0; j < group.transitions.length; j++) {
+                    if (group.transitions[j].id == self) {
+                        return group.transitions[j];
+                    }
+                }
+            }
+
+            return null;
+        },
+
+        findNeighbour(id) {
+            for (let i = 0; i < this.groups.length; i++) {
+                if (id == this.groups[i].id) {
+                    return this.groups[i];
+                }
+            }
+
+            return null;
         },
 
         groups: format(groups),
     }));
+
 };
 
 const format = data => {
@@ -25,11 +86,18 @@ const group = data => {
         screens.push(screen(data.Displays[i]));
     }
 
+    let transitions = [];
+    for (let i = 0; i < (data.Transitions || []).length; i++) {
+        transitions.push(transitionZone(data.Transitions[i]));
+    }
+
     return {
+        id: data.UUID,
         pos: new Vector(),
         width: data.Width,
         height: data.Height,
-        screens
+        transitions,
+        screens,
     };
 };
 
@@ -39,10 +107,43 @@ const screen = data => ({
     height: data.Height
 });
 
+const transitionZone = data => ({
+    id: data.UUID,
+    pos: Vector.topLeft(
+        Vector.fromGo(data.Bounds[0]), 
+        Vector.fromGo(data.Bounds[1])
+    )
+})
+
+
 class Vector {
+    static zero = new Vector();
+
+    static fromGo(goVector) {
+        return new Vector(goVector.X, goVector.Y);
+    }
+
+    static topLeft(a, b) {
+        console.log({a, ad : a.distanceFrom(Vector.zero), b, bd: b.distanceFrom(Vector.zero)});
+        if (a.distanceFrom(Vector.zero) < b.distanceFrom(Vector.zero)) {
+            return a;
+        }
+
+        return b;
+}
+
     constructor(x = 0, y = 0) {
         this.x = x;
         this.y = y;
+    }
+
+    distanceFrom(pos) {
+        return Math.sqrt(
+            Math.abs(
+                (this.x - pos.x) ** 2 
+                + (this.y - pos.y) ** 2
+            )
+        );
     }
 }
 
